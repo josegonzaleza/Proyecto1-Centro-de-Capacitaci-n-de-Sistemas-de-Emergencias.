055067bed4f7afa6c77383b7b59bb9dd60373b9f
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:14:32 -0600

    Creacion del modelo evaluacion

---
+++ CentroCapacitacionEmergencias/Models/Evaluacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class Evaluacion
+    {
+        public int Id { get; set; }
+        public int ParticipanteId { get; set; }
+        public int CursoId { get; set; }
+        public int DestrezaId { get; set; }
+        public int InstructorId { get; set; }
+        public int TiempoRespuestaSegundos { get; set; }
+        public int TiempoMinutos { get; set; }
+        public int TiempoSegundos { get; set; }
+        public decimal PuntajeOriginal { get; set; }
+        public decimal PuntajeFinal { get; set; }
+        public string AceptacionInstructor { get; set; }
+        public string Comentarios { get; set; }
+        public DateTime FechaEvaluacion { get; set; }
+        public string EstadoDominio { get; set; }
+    }
+}
---
+++ CentroCapacitacionEmergencias/Models/EvaluacionViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class EvaluacionViewModel
+    {
+        [Required]
+        public int ParticipanteId { get; set; }
+
+        [Required]
+        public int CursoId { get; set; }
+
+        [Required]
+        public int DestrezaId { get; set; }
+
+        public string ParticipanteNombre { get; set; }
+        public string CursoNombre { get; set; }
+        public string DestrezaNombre { get; set; }
+
+        [Required(ErrorMessage = "El tiempo en minutos es obligatorio.")]
+        [Range(0, 999)]
+        public int TiempoMinutos { get; set; }
+
+        [Required(ErrorMessage = "El tiempo en segundos es obligatorio.")]
+        [Range(0, 59)]
+        public int TiempoSegundos { get; set; }
+
+        [Required(ErrorMessage = "El puntaje final de ejecución es obligatorio.")]
+        [Range(typeof(decimal), "1", "100")]
+        public decimal PuntajeOriginal { get; set; }
+
+        [Required(ErrorMessage = "La aceptación del instructor es obligatoria.")]
+        [StringLength(150)]
+        public string AceptacionInstructor { get; set; }
+
+        [StringLength(500)]
+        public string Comentarios { get; set; }
+
+        public decimal PuntajeFinalCalculado { get; set; }
+        public string EstadoDominioCalculado { get; set; }
+
+        public List<PuntoControlItem> PuntosControl { get; set; }
+
+        public IEnumerable<SelectListItem> CursosDisponibles { get; set; }
+        public IEnumerable<SelectListItem> DestrezasDisponibles { get; set; }
+    }
+
+    public class PuntoControlItem
+    {
+        public int PuntoControlId { get; set; }
+        public string Descripcion { get; set; }
+
+        [Required]
+        public bool? Cumplido { get; set; }
+    }
+}
