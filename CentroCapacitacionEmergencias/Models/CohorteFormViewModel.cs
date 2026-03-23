ef15cffc78dd5ced46417689462b10a50c78409c
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:09:38 -0600

    Creacion de CohorteFormViewModel

---
+++ CentroCapacitacionEmergencias/Models/CohorteFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class CohorteFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La fecha de inicio es obligatoria.")]
+        [DataType(DataType.Date)]
+        public DateTime FechaInicio { get; set; }
+
+        [Required(ErrorMessage = "La fecha de fin es obligatoria.")]
+        [DataType(DataType.Date)]
+        public DateTime FechaFin { get; set; }
+
+        public bool Archivado { get; set; }
+    }
+}
