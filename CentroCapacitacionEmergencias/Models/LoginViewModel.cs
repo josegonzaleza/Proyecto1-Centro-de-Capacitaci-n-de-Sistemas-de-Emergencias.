b1621ffb759e59f04d9b105eb4033da445743a84
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:15:51 -0600

    Creacion del modelo del login

---
+++ CentroCapacitacionEmergencias/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El usuario es obligatorio.")]
+        [Display(Name = "Usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
+}
