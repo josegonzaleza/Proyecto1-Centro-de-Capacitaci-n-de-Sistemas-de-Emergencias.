f2c5daf258b5c1a63b3378f54d1c99dbb4955713
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:15:06 -0600

    Creacion del modelo intervencion

---
+++ CentroCapacitacionEmergencias/Models/IntervencionPendienteItem.cs
@@ -0,0 +1,14 @@
+﻿namespace CentroCapacitacionEmergencias.Models
+{
+    public class IntervencionPendienteItem
+    {
+        public int ParticipanteId { get; set; }
+        public string CodigoInterno { get; set; }
+        public string NombreCompleto { get; set; }
+        public string CursoTitulo { get; set; }
+        public string CohorteNombre { get; set; }
+        public decimal HorasCompletadas { get; set; }
+        public int HorasRequeridas { get; set; }
+        public decimal PorcentajeCumplimiento { get; set; }
+    }
+}
