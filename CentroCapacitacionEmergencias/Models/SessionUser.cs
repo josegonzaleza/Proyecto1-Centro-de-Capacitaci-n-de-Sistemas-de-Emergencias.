1c0312ddde61253aa57a834ab700c41d25707338
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:23:40 -0600

    Creacion de los modelos de usuario y session de usuario

---
+++ CentroCapacitacionEmergencias/Models/SessionUser.cs
@@ -0,0 +1,10 @@
+﻿namespace CentroCapacitacionEmergencias.Models
+{
+    public class SessionUser
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Username { get; set; }
+        public string Rol { get; set; }
+    }
+}
---
+++ CentroCapacitacionEmergencias/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public int RolId { get; set; }
+        public string RolNombre { get; set; }
+        public bool Activo { get; set; }
+        public int IntentosFallidos { get; set; }
+        public DateTime? BloqueadoHasta { get; set; }
+    }
+}
