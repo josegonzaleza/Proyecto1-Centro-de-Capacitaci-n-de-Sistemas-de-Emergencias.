2991c86a960ce5d99e32d7f313cd9163eecb687c
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:13:13 -0600

    Creacion del modelo destreza

---
+++ CentroCapacitacionEmergencias/Models/Destreza.cs
@@ -0,0 +1,11 @@
+﻿namespace CentroCapacitacionEmergencias.Models
+{
+    public class Destreza
+    {
+        public int Id { get; set; }
+        public int CursoId { get; set; }
+        public string Nombre { get; set; }
+        public decimal NotaMinimaAprobacion { get; set; }
+        public bool Activa { get; set; }
+    }
+}
---
+++ CentroCapacitacionEmergencias/Models/DestrezaAjaxItem.cs
@@ -0,0 +1,8 @@
+﻿namespace CentroCapacitacionEmergencias.Models
+{
+    public class DestrezaAjaxItem
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
