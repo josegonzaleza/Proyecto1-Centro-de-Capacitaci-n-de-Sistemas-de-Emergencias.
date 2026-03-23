0addf7fd69f4a832d2544d9403dd91b3009d50fd
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:11:30 -0600

    Creacion del modelo CursoFormViewModel

---
+++ CentroCapacitacionEmergencias/Models/CursoFormViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class CursoFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El código es obligatorio.")]
+        [StringLength(30)]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio.")]
+        [StringLength(150)]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Las horas prácticas son obligatorias.")]
+        [Range(1, 10000)]
+        public int TotalHorasPracticas { get; set; }
+
+        public bool Archivado { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar al menos un instructor.")]
+        public List<int> InstructoresIds { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar al menos una cohorte.")]
+        public List<int> CohortesIds { get; set; }
+
+        public IEnumerable<SelectListItem> Instructores { get; set; }
+        public IEnumerable<SelectListItem> Cohortes { get; set; }
+    }
+}
