6446c799e0a64e3cfb9f3dab999e96a27e225dc2
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:22:01 -0600

    Creacion de modelo de riesgo

---
+++ CentroCapacitacionEmergencias/Models/PuntoControlAjaxItem.cs
@@ -0,0 +1,8 @@
+﻿namespace CentroCapacitacionEmergencias.Models
+{
+    public class PuntoControlAjaxItem
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ CentroCapacitacionEmergencias/Models/RiesgoDetalleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class RiesgoDetalleViewModel
+    {
+        public int DestrezaId { get; set; }
+        public string DestrezaNombre { get; set; }
+        public List<RiesgoParticipanteItem> Participantes { get; set; }
+    }
+
+    public class RiesgoParticipanteItem
+    {
+        public int ParticipanteId { get; set; }
+        public string CodigoInterno { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Identificacion { get; set; }
+        public decimal UltimoPuntajeFinal { get; set; }
+        public string EstadoDominio { get; set; }
+        public string CursoTitulo { get; set; }
+        public string CohorteNombre { get; set; }
+        public string FechaEvaluacionTexto { get; set; }
+        public string Comentarios { get; set; }
+    }
+}
