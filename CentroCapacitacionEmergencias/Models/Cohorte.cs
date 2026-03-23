a17997e38ef85b274eaa24b9f77cdbd3648cbc04
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:08:07 -0600

    Creacion del modelo de cohorte

---
+++ CentroCapacitacionEmergencias/Models/Cohorte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CentroCapacitacionEmergencias.Models
+{
+    public class Cohorte
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public bool Archivado { get; set; }
+        public bool Activo { get; set; }
+    }
+}
