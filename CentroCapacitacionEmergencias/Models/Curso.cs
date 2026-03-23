ee49a32d05c7a67ab31a588610c6f3c41d38f300
josegonzaleza <josegonzaleza@uned.cr>
2026-03-23 01:10:49 -0600

    Creacion del modelo de curso

---
+++ CentroCapacitacionEmergencias/Models/Curso.cs
@@ -0,0 +1,12 @@
+﻿namespace CentroCapacitacionEmergencias.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public int TotalHorasPracticas { get; set; }
+        public bool Archivado { get; set; }
+        public bool Activo { get; set; }
+    }
+}
